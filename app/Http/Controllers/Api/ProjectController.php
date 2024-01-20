7c962f5d9e4e2f28be7bec78912d9ee38a89b95f
Trotteriniku <stefanozidda3@gmail.com>
2024-01-20 15:39:30 +0100

    MILESTONE 1 and 2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Technology;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::paginate(3);
+        return response()->json($projects);
+    }
+
+    public function show($slug)
+    {
+        $projects = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
+        return response()->json($projects);
+
+
+    }
+}
