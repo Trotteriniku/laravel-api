d1e2b23bac144b7af863471c429d7a5959ddb4ed
Trotteriniku <stefanozidda3@gmail.com>
2024-01-25 09:09:05 +0100

    created ne email controller and api

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>CIao Admin</h1>
+    <p>hai ricevuto un messaggio ecco i dettagli <br>
+        - Nome:{{ $lead->name }} <br>
+        - Email:{{ $lead->email }} <br>
+        - Address:{{ $lead->address }} <br>
+        - Message:{{ $lead->message }}
+    </p>
+</div>
